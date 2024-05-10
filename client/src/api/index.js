52c8d19c0bbd52074a3cbba5dc91bc74732d0dfb
manumn95 <manumnofficial.95@gmail.com>
2024-05-10 12:03:01 +0530

    changes made

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https:nutrifit-capstone-projectonrendercom/api",
+  baseURL: "https://capstone-project-fitness.onrender.com/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
